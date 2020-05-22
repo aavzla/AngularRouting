0805cc9e692b415d1dfe39fe7fa208f34c32fba7
AAvilan <andres.avilan@nurun.com>
2020-05-22 15:45:54 -0400

    Using Relative Paths in Programmatic Navigation - If we use relative paths with programmatic navigation, we must past to where it is relative to. In this case, we set up a button to call the servers and if we didn't pass the second argument, it doesn't know therefore it won't launch an error. Here we comment on the code because it will try to find a route that does not exist.

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ServersService } from './servers.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-servers',
@@ -7,12 +8,32 @@ import { ServersService } from './servers.service';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-  public servers: {id: number, name: string, status: string}[] = [];
+  public servers: { id: number, name: string, status: string }[] = [];
 
-  constructor(private serversService: ServersService) { }
+  constructor(
+    private serversService: ServersService,
+    private router: Router,
+    //Activated Route give us access to the route loaded for this component.
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
   }
 
+  onReload() {
+    /*
+     * We are using a relative path here.
+     * The router navigate method doesn't know on which route you are currently on.
+     * Therefore, we neeed to pass a second argument to give the reference.
+     * Here, we inject and add the activated route to the navigate method.
+     */
+
+    /*
+     * In this case, the navigate route will try to go ./servers/servers.
+     * Therefore, it will break because it doesn't exists.
+     * So we comment it out.
+     */
+    //this.router.navigate(['servers'], { relativeTo: this.route });
+  }
 }
