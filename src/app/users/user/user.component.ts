8e6da17fdf81149eb5db8f218e6600445d4a6557
AAvilan <andres.avilan@nurun.com>
2020-05-25 14:28:01 -0400

    An Important Note about Route Observables - Implementing the handling of a subscription for unsubscribe and free memory when the component is destroyed. When subscribing to an observable in a component, you almost always unsubscribe when the component is destroyed. This should be implemented for all subscriptions. In this case,  ActivatedRoute observables are among the exceptions because ActivatedRoute and its observables are insulated from the Router itself. The Router destroys a routed component when it is no longer needed along with the injected ActivatedRoute.

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,13 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: { id: number, name: string };
+  //This subscription from Rxjs will hold the subscription so we can unsubscribe when this component is destroyed.
+  //This is optional in this case. Angular will unsubscribe it for you because this subscription is part of his library.
+  //If you create a custom subscription, you will need to handle the unsubscribe with the OnDestroy phase.
+  //Please visit, https://angular.io/guide/router#observable-parammap-and-component-reuse.
+  paramsSubscription: Subscription
 
   constructor(private route: ActivatedRoute) { }
 
@@ -16,11 +22,18 @@ export class UserComponent implements OnInit {
       id: this.route.snapshot.params['id'],
       name: this.route.snapshot.params['name']
     };
-    this.route.params.subscribe(
+
+    this.paramsSubscription = this.route.params.subscribe(
       (params: Params) => {
         this.user.id = params['id'];
         this.user.name = params['name'];
       }
     );
+  }
+
+  ngOnDestroy() {
+    //This log is for checking purposes of execution.
+    //console.log(this.constructor.name + ' OnDestroy - unsubscribed the params for route.');
+    this.paramsSubscription.unsubscribe();
   }
 }
