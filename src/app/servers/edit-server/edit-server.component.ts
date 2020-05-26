3c3f3d6544cda056ea58ec2fbc8f58589bbe9b37
AAvilan <andres.avilan@nurun.com>
2020-05-26 10:42:00 -0400

    Practicing and some Common Gotchas - Important to notice the type received and sent to avoid errors. Also, check if all required parameters for a component is met, even if it is a child component of another. In this case, we added a route to the server component, modify the routerLink of the users and server components to display the single element and not the edit in the case of the server. The server component element in the servers HTML was commented to avoid errors of not configuring the nested components in the routing.

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -21,11 +21,11 @@ export class EditServerComponent implements OnInit {
   ngOnInit() {
     //these logs are to prove the state of the snapshot queryParams and Fragment against their direct access.
     //This is how we can retreive Query parameters and Fragments from the active route.
-    console.log(this.constructor.name + ' OnInit - Snapshot of QueryParams.', this.route.snapshot.queryParams);
-    console.log(this.constructor.name + ' OnInit - Snapshot of Fragment.', this.route.snapshot.fragment);
+    //console.log(this.constructor.name + ' OnInit - Snapshot of QueryParams.', this.route.snapshot.queryParams);
+    //console.log(this.constructor.name + ' OnInit - Snapshot of Fragment.', this.route.snapshot.fragment);
 
-    console.log(this.constructor.name + ' OnInit - QueryParams.', this.route.queryParams);
-    console.log(this.constructor.name + ' OnInit - Fragment.', this.route.fragment);
+    //console.log(this.constructor.name + ' OnInit - QueryParams.', this.route.queryParams);
+    //console.log(this.constructor.name + ' OnInit - Fragment.', this.route.fragment);
 
     //This is the alternative way to have access to the changes in these query params and fragments, to subscribe.
     //This will be used later, so i will comment these for now.
