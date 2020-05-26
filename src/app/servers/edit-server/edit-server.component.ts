d56efb6855381e6cd4ec250a0ff091f69a344ab1
AAvilan <andres.avilan@nurun.com>
2020-05-26 08:36:36 -0400

    Retrieving Query Parameters and Fragments - Getting the query params and Fragments from the active route. In this case, we got them from the edit-server component and display them on the console.

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-edit-server',
@@ -12,9 +13,25 @@ export class EditServerComponent implements OnInit {
   serverName = '';
   serverStatus = '';
 
-  constructor(private serversService: ServersService) { }
+  constructor(
+    private serversService: ServersService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    //these logs are to prove the state of the snapshot queryParams and Fragment against their direct access.
+    //This is how we can retreive Query parameters and Fragments from the active route.
+    console.log(this.constructor.name + ' OnInit - Snapshot of QueryParams.', this.route.snapshot.queryParams);
+    console.log(this.constructor.name + ' OnInit - Snapshot of Fragment.', this.route.snapshot.fragment);
+
+    console.log(this.constructor.name + ' OnInit - QueryParams.', this.route.queryParams);
+    console.log(this.constructor.name + ' OnInit - Fragment.', this.route.fragment);
+
+    //This is the alternative way to have access to the changes in these query params and fragments, to subscribe.
+    //This will be used later, so i will comment these for now.
+    //this.route.queryParams.subscribe();
+    //this.route.fragment.subscribe();
+
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
