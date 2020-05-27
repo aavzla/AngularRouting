78ccc92996744ac1b76ed9682ecdb22ae7a4fb03
AAvilan <andres.avilan@nurun.com>
2020-05-27 12:02:04 -0400

    Outsourcing the Route Configuration - Export the route config from the main module to another module and import the config into the main. This will help to have a scalable and clean project.

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,59 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { UsersComponent } from "./users/users.component";
+import { UserComponent } from "./users/user/user.component";
+import { ServersComponent } from "./servers/servers.component";
+import { ServerComponent } from "./servers/server/server.component";
+import { EditServerComponent } from "./servers/edit-server/edit-server.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  {
+    path: 'users', component: UsersComponent, children: [
+      { path: ':id/:name', component: UserComponent },
+    ]
+  },
+  {
+    path: 'servers', component: ServersComponent, children: [
+      { path: ':id', component: ServerComponent },
+      { path: ':id/edit', component: EditServerComponent }
+    ]
+  },
+  //This is the component for non existing routes found. (404 error)
+  { path: 'not-found', component: PageNotFoundComponent },
+  /*
+   * This must be always the last route in the configurations of the routes,
+   * because all configuration routes are registred from top to bottom and
+   * for example if this is at the beggining, all routes will be redirected.
+   *
+   * Here we use wildcard ** to express anything typed in the URL (Alphanumeric, special chars, etc.)
+   */
+  { path: '**', redirectTo: '/not-found' },
+
+  /*
+   * See bellow, these are the cases to test redirection.
+   * If we comment the first path(with the path ''), try to use any of the redirections bellow(one at a time).
+  */
+
+  /*
+   * This is the case without the pathMatch option, this will throw the following error:
+   * Error: Invalid configuration of route '{path: "", redirectTo: "/not-found"}':please provide 'pathMatch'.
+   * The default value of 'pathMatch' is 'prefix', but often the intent is to use 'full'.
+  */
+  //{ path: '', redirectTo: '/not-found' }
+
+  //This is the case with pathMatch option, please visit https://angular.io/api/router/Route#pathMatch.
+  //{ path: '', redirectTo: '/not-found', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(appRoutes)
+  ],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule } from "@angular/router"
+import { AppRoutingModule } from './app-routing.module';
 
 
 import { AppComponent } from './app.component';
@@ -14,45 +14,7 @@ import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const appRoutes: Routes = [
-  { path: '', component: HomeComponent },
-  {
-    path: 'users', component: UsersComponent, children: [
-      { path: ':id/:name', component: UserComponent },
-    ]
-  },
-  {
-    path: 'servers', component: ServersComponent, children: [
-      { path: ':id', component: ServerComponent },
-      { path: ':id/edit', component: EditServerComponent }
-    ]
-  },
-  //This is the component for non existing routes found. (404 error)
-  { path: 'not-found', component: PageNotFoundComponent },
-  /*
-   * This must be always the last route in the configurations of the routes,
-   * because all configuration routes are registred from top to bottom and
-   * for example if this is at the beggining, all routes will be redirected.
-   *
-   * Here we use wildcard ** to express anything typed in the URL (Alphanumeric, special chars, etc.)
-   */
-  { path: '**', redirectTo: '/not-found' },
-
-  /*
-   * See bellow, these are the cases to test redirection.
-   * If we comment the first path(with the path ''), try to use any of the redirections bellow(one at a time).
-  */
-
-  /*
-   * This is the case without the pathMatch option, this will throw the following error:
-   * Error: Invalid configuration of route '{path: "", redirectTo: "/not-found"}':please provide 'pathMatch'.
-   * The default value of 'pathMatch' is 'prefix', but often the intent is to use 'full'.
-  */
-  //{ path: '', redirectTo: '/not-found' }
-
-  //This is the case with pathMatch option, please visit https://angular.io/api/router/Route#pathMatch.
-  //{ path: '', redirectTo: '/not-found', pathMatch: 'full' }
-];
+//The const appRoutes was transfered to the app-routing module and i made this comment to have trace history.
 
 @NgModule({
   declarations: [
@@ -68,7 +30,7 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
